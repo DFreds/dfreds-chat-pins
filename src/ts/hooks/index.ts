e8afc44e55065e373a95bbd926021e6cffb3ba9b
Derek Fredrickson <dfreds08@gmail.com>
2025-03-28 16:41:09 -0400

    Add debug hooks

--- src/ts/hooks/index.ts
+++ src/ts/hooks/index.ts
@@ -3,6 +3,7 @@ import { Ready } from "./ready.ts";
 import { RenderChatLog } from "./renderChatLog.ts";
 import { UpdateChatMessage } from "./updateChatMessage.ts";
 import { DeleteChatMessage } from "./deleteChatMessage.ts";
+import { Setup } from "./setup.ts";
 import { GetChatMessageContextOptions } from "./getChatMessageContextOptions .ts";
 import { RenderChatMessageHTML } from "./renderChatMessageHTML.ts";
 
@@ -14,6 +15,7 @@ const HooksChatPins = {
     listen(): void {
         const listeners: Listener[] = [
             Init,
+            Setup,
             Ready,
             RenderChatMessageHTML,
             RenderChatLog,
